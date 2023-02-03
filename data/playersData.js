fa997cd28e1d3d80fa1f66f78ac1087c56a61a35
Ashley Richard <ashleymrichard@gmail.com>
2023-02-02 23:23:38 -0800

    adds seed

--- data/playersData.js
+++ data/playersData.js
@@ -3,8 +3,27 @@ const playersData = [
         name: "Carson Shaw",
         image: "/images/Carson.jpg",
         position: "Catcher",
-        team: "Rockford Peaches",
+        team: "/images/peaches.png",
     },
+    {
+        name: "Maxine Chapman",
+        image: "/images/Max.jpg",
+        position: "Pitcher",
+        team: "/images/Red.png",
+    },
+    {
+        name: "Greta Gill",
+        image: "/images/Greta.jpg",
+        position: "First Baseman",
+        team: "/images/peaches.png",
+    },
+    {
+        name: "Lupe García",
+        image: "/images/Lupe.jpg",
+        position: "Pitcher",
+        team: "/images/peaches.png",
+    },
+
 ]
 
 module.exports = playersData;
